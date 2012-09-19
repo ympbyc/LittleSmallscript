59aa4ee8a0f56325f3df3fd95ce397ed2ee12f4d
Minori Yamashita <ympbyc@gmail.com>
2012-09-19 09:44:43 +0900

    "@foo" compiles into "this.foo"

--- src/littleparsers.js
+++ src/littleparsers.js
@@ -173,8 +173,9 @@
     LittleParsers.prototype.variable = function () {
       var _this = this;
       return this.cacheDo("variable", function () {
-        var v = _this.regex(/^[a-zA-Z_$][a-zA-Z0-9_$]*/);
+        var v = _this.regex(/^[a-zA-Z_$@][a-zA-Z0-9_$]*/);
         if (v === 'self') return 'this';
+        if (v[0] === '@') return 'this.'+v.substring(1); //@foo -> this.foo
         return v;
       });
     };
